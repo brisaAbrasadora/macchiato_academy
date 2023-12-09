96ce87bb9e05f8afe5dba330b90d7e8f433fff1e
Daniel Rabago Prades <danielrabagoprades@gmail.com>
2023-12-09 02:03:31 +0100

    Start

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Ejercicios tema 3 - 1</title>
+	</head>
+	<body>
+		<?php
+			echo "<p>1. Cadenas</p>";
+			
+		?>
+		
+	</body>
+</html>
